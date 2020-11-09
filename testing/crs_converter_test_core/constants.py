e142b4a7ed9f2cd17b7278941c41731fa7ad090a
Walter D <walterdsouza@in.ibm.com>
2020-11-09 03:36:26 -0500

    Ibm implementation

--- testing/crs_converter_test_core/constants.py
+++ testing/crs_converter_test_core/constants.py
@@ -1,7 +1,11 @@
 import os
 
+VENDOR = os.getenv("VENDOR")
 BASE_URL = os.getenv("BASE_URL", '/api/crs/converter/v2')
-ROOT_URL = os.getenv("VIRTUAL_SERVICE_HOST_NAME")
+if VENDOR == 'ibm':
+    ROOT_URL = os.getenv("IBM_VIRTUAL_HOST_CRS_CONVERSION")
+else:
+    ROOT_URL = os.getenv("VIRTUAL_SERVICE_HOST_NAME")
 DATA_DIR = os.getenv("DATA_DIR")
 DATA_PATTERN = os.getenv("DATA_PATTERN")
 REPORT_PATH = os.getenv("REPORT_PATH")
