7d2b423cec43a7d8ed25136db84dcff292c68b03
Spencer Sutton <suttonsp@amazon.com>
2021-06-15 10:53:44 -0500

    Crs conversion was cycling, needed to scan core common packages

--- provider/crs-converter-aws/src/main/java/org/opengroup/osdu/crs/CrsApplicationAWS.java
+++ provider/crs-converter-aws/src/main/java/org/opengroup/osdu/crs/CrsApplicationAWS.java
@@ -16,8 +16,10 @@ package org.opengroup.osdu.crs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"org.opengroup.osdu"})
 public class CrsApplicationAWS {
 
     public static void main(String[] args) {
