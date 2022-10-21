df2b3a9071905902776899c2abcfb417c9b814e2
Puneet Bhardwaj <puneet.bhardwaj@shell.com>
2022-10-21 12:52:46 +0000

    Update AbstractBinGrid.java

--- crs-converter-core/src/main/java/org/opengroup/osdu/crs/BinGrid/AbstractBinGrid.java
+++ crs-converter-core/src/main/java/org/opengroup/osdu/crs/BinGrid/AbstractBinGrid.java
@@ -1,6 +1,6 @@
 package org.opengroup.osdu.crs.BinGrid;
 
-import java.util.Arrays;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -61,27 +61,9 @@ public class AbstractBinGrid {
 	private Double p6BinNodeIncrementOnJaxis;
 
 	@JsonProperty("ABCDBinGridLocalCoordinates")
-	private AbstractCoordinates[] aBCDBinGridLocalCoordinates;
-	 
+	private List<AbstractCoordinates> aBCDBinGridLocalCoordinates;
+
 	@JsonProperty("ABCDBinGridSpatialLocation")
-	 private AbstractSpatialLocation aBCDBinGridSpatialLocation;
-
-	
-
-	@Override
-	public String toString() {
-		return "AbstractBinGrid [binGridName=" + binGridName + ", binGridTypeID=" + binGridTypeID + ", sourceBinGridID="
-				+ sourceBinGridID + ", sourceBinGridAppID=" + sourceBinGridAppID + ", soveragePercent="
-				+ coveragePercent + ", binGridDefinitionMethodTypeID=" + binGridDefinitionMethodTypeID
-				+ ", p6TransformationMethod=" + p6TransformationMethod + ", p6BinGridOriginI=" + p6BinGridOriginI
-				+ ", p6BinGridOriginJ=" + p6BinGridOriginJ + ", p6BinGridOriginEasting=" + p6BinGridOriginEasting
-				+ ", p6BinGridOriginNorthing=" + p6BinGridOriginNorthing + ", p6ScaleFactorOfBinGrid="
-				+ p6ScaleFactorOfBinGrid + ", p6BinWidthOnIaxis=" + p6BinWidthOnIaxis + ", p6BinWidthOnJaxis="
-				+ p6BinWidthOnJaxis + ", p6MapGridBearingOfBinGridJaxis=" + p6MapGridBearingOfBinGridJaxis
-				+ ", p6BinNodeIncrementOnIaxis=" + p6BinNodeIncrementOnIaxis + ", p6BinNodeIncrementOnJaxis="
-				+ p6BinNodeIncrementOnJaxis + ", aBCDBinGridLocalCoordinates="
-				+ Arrays.toString(aBCDBinGridLocalCoordinates) + ", aBCDBinGridSpatialLocation="
-				+ aBCDBinGridSpatialLocation + "]";
-	}
+	private AbstractSpatialLocation aBCDBinGridSpatialLocation;
 
 }
