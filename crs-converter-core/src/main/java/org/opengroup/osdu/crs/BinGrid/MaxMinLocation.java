398d7470d9f073a7c1fb77c2592cc876ecf103c1
kiranallamsety <kiran.allamsety@shell.com>
2022-10-27 12:33:35 +0530

    bin grid code updates

--- crs-converter-core/src/main/java/org/opengroup/osdu/crs/BinGrid/MaxMinLocation.java
+++ crs-converter-core/src/main/java/org/opengroup/osdu/crs/BinGrid/MaxMinLocation.java
@@ -1,11 +1,12 @@
 package org.opengroup.osdu.crs.BinGrid;
 
+import lombok.Data;
+
+@Data
 public class MaxMinLocation {
 		
 	private Double dI;
 	private Double dJ;
-
-
 	
 	@Override
 	public String toString() {
