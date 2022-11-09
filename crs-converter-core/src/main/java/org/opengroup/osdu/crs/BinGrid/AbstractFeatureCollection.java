075d1d45def64b43ef9b3a1b8e89541cecbf3597
kiranallamsety <kiran.allamsety@shell.com>
2022-11-09 17:04:11 +0530

    code updates for bin grid

--- crs-converter-core/src/main/java/org/opengroup/osdu/crs/BinGrid/AbstractFeatureCollection.java
+++ crs-converter-core/src/main/java/org/opengroup/osdu/crs/BinGrid/AbstractFeatureCollection.java
@@ -1,5 +1,7 @@
 package org.opengroup.osdu.crs.BinGrid;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
@@ -8,6 +10,6 @@ import lombok.Data;
 public class AbstractFeatureCollection {
 	
 	@JsonProperty("features")
-    private AbstractFeature[] features;
+    private List<AbstractFeature> features;
 
 }
