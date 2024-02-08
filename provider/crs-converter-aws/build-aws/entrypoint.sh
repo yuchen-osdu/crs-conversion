5d59e1ef3d3429c069164c09defe86901d71f7db
NikhilPatil <nikhil.patil5@ibm.com>
2024-02-08 14:07:33 +0530

    update to java 17

--- provider/crs-converter-aws/build-aws/entrypoint.sh
+++ provider/crs-converter-aws/build-aws/entrypoint.sh
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-java $JAVA_OPTS -jar /app.jar
+java $JAVA_OPTS --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED -jar /app.jar
