365e9bd3e32bda2cc144068dc41a95b7ea55f87f
Thulasi Dass Subramanian <thulasi_dass_subramanian@epam.com>
2024-03-26 12:37:22 +0000

    [#MSOSDU-36917] upgrade Azure dependencies - Spring Boot 3 + Spring 6 Migration

--- provider/crs-converter-azure/crs-converter-aks/src/main/java/org/opengroup/osdu/crs/security/AzureIstioSecurityConfig.java
+++ provider/crs-converter-azure/crs-converter-aks/src/main/java/org/opengroup/osdu/crs/security/AzureIstioSecurityConfig.java
@@ -16,17 +16,32 @@ package org.opengroup.osdu.crs.security;
 
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
-
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
 @ConditionalOnProperty(value = "azure.istio.auth.enabled", havingValue = "true", matchIfMissing = true)
-public class AzureIstioSecurityConfig extends WebSecurityConfigurerAdapter {
+public class AzureIstioSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable()
-                .csrf().disable();  //AuthN is disabled. AuthN is handled by sidecar proxy
+    @Bean
+    public SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .httpBasic(withDefaults());
+        return http.build();
     }
 }
