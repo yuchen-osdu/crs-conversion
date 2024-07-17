ae177fa9417c5cf2d4536648b8c43d4c9ddcecfa
Stanislav_Riabokon@epam.com <Stanislav_Riabokon@epam.com>
2024-07-17 09:03:21 +0300

    Fixed endpoints

--- crs-converter-core-plus/src/main/java/org/opengroup/osdu/crs/security/AuthSecurityConfig.java
+++ crs-converter-core-plus/src/main/java/org/opengroup/osdu/crs/security/AuthSecurityConfig.java
@@ -1,38 +1,38 @@
 /*
-  Copyright 2020 Google LLC
-  Copyright 2020 EPAM Systems, Inc
+ Copyright 2020-2024 Google LLC
+ Copyright 2020-2024 EPAM Systems, Inc
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+   http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
- */
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 
 package org.opengroup.osdu.crs.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import org.opengroup.osdu.core.common.model.http.AppError;
 import org.opengroup.osdu.crs.middleware.AuthenticationRequestFilter;
 import org.opengroup.osdu.crs.middleware.AuthenticationService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -41,81 +41,96 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
-@EnableMethodSecurity
-public class AuthSecurityConfig implements AccessDeniedHandler,
-    AuthenticationEntryPoint {
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AuthSecurityConfig implements AccessDeniedHandler, AuthenticationEntryPoint {
 
-    private AuthenticationRequestFilter authFilter;
+  private AuthenticationRequestFilter authFilter;
 
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    private static final String[] AUTH_WHITELIST = {
-        "/",
-        "/actuator/**",
-        "/_ah/*",
-        "/v2/api-docs",
-        "/v3/api-docs",
-        "/api-docs/**",
-        "/configuration/ui",
-        "/swagger-resources/**",
-        "/configuration/security",
-        "/swagger-ui.html",
-        "/swagger-ui/**",
-        "/api-docs/swagger-config",
-        "/v2/info",
-        "/webjars/**",
-        "/csrf",
-        "/api/crs/converter/actuator",
-        "/api/crs/converter/actuator/**",
-        "/api/crs/converter/actuator/health",
-    };
+  private static final String[] AUTH_WHITELIST = {
+    "/",
+    "/actuator/**",
+    "/_ah/*",
+    "/v2/api-docs",
+    "/v3/api-docs",
+    "/api-docs/**",
+    "/configuration/ui",
+    "/swagger-resources/**",
+    "/configuration/security",
+    "/swagger-ui.html",
+    "/swagger-ui/**",
+    "/api-docs/swagger-config",
+    "/v2/info",
+    "/webjars/**",
+    "/csrf",
+    "/api/crs/converter/actuator",
+    "/api/crs/converter/actuator/**",
+    "/api/crs/converter/actuator/health",
+    "/health/readiness",
+    "/health/liveness"
+  };
 
-    //AuthenticationRequestFilter is not a recognized bean, so construct it manually
-    public AuthSecurityConfig(AuthenticationService service) {
-        authFilter = new AuthenticationRequestFilter(service);
-    }
+  // AuthenticationRequestFilter is not a recognized bean, so construct it manually
+  public AuthSecurityConfig(AuthenticationService service) {
+    authFilter = new AuthenticationRequestFilter(service);
+  }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement((sess) -> sess.sessionCreationPolicy(SessionCreationPolicy.NEVER))
-                .authorizeHttpRequests(request -> request.requestMatchers(AUTH_WHITELIST).permitAll())
-                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
-    }
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+            authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers(AUTH_WHITELIST)
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .sessionManagement(
+            sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.NEVER))
+        .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+        .csrf(csrf -> csrf.disable());
+    return http.build();
+  }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(AUTH_WHITELIST);
-    }
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return web -> web.ignoring().requestMatchers(AUTH_WHITELIST);
+  }
 
-    @Override
-    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
-        AccessDeniedException e) throws IOException {
-        writeUnauthorizedError(httpServletResponse);
-    }
+  @Override
+  public void handle(
+      HttpServletRequest httpServletRequest,
+      HttpServletResponse httpServletResponse,
+      AccessDeniedException e)
+      throws IOException {
+    writeUnauthorizedError(httpServletResponse);
+  }
 
-    @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-        AuthenticationException authException) throws IOException {
-        writeUnauthorizedError(response);
-    }
+  @Override
+  public void commence(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AuthenticationException authException)
+      throws IOException {
+    writeUnauthorizedError(response);
+  }
 
-    private static void writeUnauthorizedError(HttpServletResponse response) throws IOException {
-        AppError appError = AppError.builder()
+  private static void writeUnauthorizedError(HttpServletResponse response) throws IOException {
+    AppError appError =
+        AppError.builder()
             .code(HttpStatus.UNAUTHORIZED.value())
             .message("The user is not authorized to perform this action")
             .reason("Unauthorized")
             .build();
-        String body = OBJECT_MAPPER.writeValueAsString(appError);
+    String body = OBJECT_MAPPER.writeValueAsString(appError);
 
-        PrintWriter out = response.getWriter();
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        out.print(body);
-        out.flush();
-    }
+    PrintWriter out = response.getWriter();
+    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    out.print(body);
+    out.flush();
+  }
 }
