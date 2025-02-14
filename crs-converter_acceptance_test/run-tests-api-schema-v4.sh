011c9306f82336f012a94e69b928beff41f466d5
Paulo Oliveira <paolive@microsoft.com>
2025-02-14 22:45:12 +0000

    Add acceptance tests and demo data

---
+++ crs-converter_acceptance_test/run-tests-api-schema-v4.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+python3 -m pip install --upgrade pip
+
+python3 -m venv env
+source env/bin/activate
+
+python3 -m pip install -r requirements.txt
+
+echo ""
+export API_VER="v4"
+echo ***RUNNING CATALOG API $API_VER SCHEMA TESTS***
+pytest -v test_api_v4.py
+TEST_STATUS=$?
+echo ***FINISHED CATALOG API $API_VER SCHEMA TESTS***
+
+echo "TEST STATUS: $TEST_STATUS"
+
+deactivate
+rm -rf env/
+
+if [ $TEST_STATUS -ne 0 ]
+then
+    exit 1
+fi
